fd7846750bbc2397c3d709b33147dfadde2a23e7
nfichter <nfichter@stuy.edu>
2017-01-06 17:13:30 -0500

    may have to rework paypal entirely (they dont really support their API but they support their library) - talking to mr brown

--- utils/paypal.py
+++ utils/paypal.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import json
 
 client_id="AQSIxUH2ndmMMKY6dUDu8BSbWZ9v77R7PGbW2zDxzluD3DL5mI6YBA2LZVrBtqgGE331-ZLx1PJPqKXc"
 client_secret="ECzSK7bvA-M9_6vDuQaS7QHQpnW3WRWHQxxsewNPQ326ZhCsCjmoygbYkalhmx_WX9mcpnON_GSnFlpI"
@@ -37,7 +38,7 @@ class Seller:
 		#"quantity" - number of items (most of the time this will be "1")
 		#"unit_price" - dictionary that contains two keys:
 			#"currency" - always "USD"
-			#"value" - price in dollars
+			#"value" - price in dollars (INTEGER)
 	#email, fname, lname, business_name, and note (use get_note() for this) will be strings
 	def __init__(self, email, fname, lname, business_name, phone, address, item_info, note):
 		self.email = email
@@ -80,16 +81,15 @@ def get_token():
 	}
 
 	token = requests.post("https://api.sandbox.paypal.com/v1/oauth2/token", headers=headers, data=param)
-
-	return token["access_token"]
+	return json.loads(token.text)["access_token"]
 	
-def create_invoice(seller_obj, buyer_obj, item_obj, access_token):
+def create_invoice(seller_obj, buyer_obj, access_token):
 	headers = {
     	'Content-Type': 'application/json',
     	'Authorization': 'Bearer %s' % access_token,
 	}
 
-	data = {
+	payload = {
 		"merchant_info": {
 			"email": seller_obj.email,
 			"first_name": seller_obj.fname,
@@ -98,9 +98,9 @@ def create_invoice(seller_obj, buyer_obj, item_obj, access_token):
   			"phone": seller_obj.phone,
   			"address": seller_obj.address
   		},
-  		"billing_info": buyer_obj.billing_info
+  		"billing_info": buyer_obj.billing_info,
   		"items": seller_obj.item_info,
-  		"note": seller_obj.item_note,
+  		"note": seller_obj.note,
   		"payment_term": {"term_type": "NET_45"},
   		"shipping_info": {
   			"first_name": buyer_obj.fname,
@@ -111,36 +111,56 @@ def create_invoice(seller_obj, buyer_obj, item_obj, access_token):
 		}
 	}
 	
-	result = requests.post('https://api.sandbox.paypal.com/v1/invoicing/invoices/', headers=headers, data=data)
-	print result
+	result = requests.post('https://api.sandbox.paypal.com/v1/invoicing/invoices/', headers=headers, data=payload)
+	print result.json()
 	
 #EXAMPLE/TEST
 #Note that these will be automatically created in the database and simply need to be pulled
 #from the DB once it is implemented
-seller_email = "yabeapplication_facilitator@gmail.com"
+seller_email = "yabeapplication-facilitator-1@gmail.com"
 seller_fname = "Yabe"
-seller_lname = "App"
+seller_lname = "Facilitator"
 seller_business_name = "YabeApplication"
 seller_phone = {
-	"country_code" = "001",
-	"national_number" = "1234567890"
+	"country_code": "001",
+	"national_number": "1234567890"
 }
 seller_address = {
-	"line1" = "345 Chambers Street",
-	"line2" = "",
-	"city" = "New York",
-	"state" = "NY",
-	"postal_code" = "10282",
-	"country_code" = "US"
+	"line1": "345 Chambers Street",
+	"line2": "",
+	"city": "New York",
+	"state": "NY",
+	"postal_code": "10282",
+	"country_code": "US"
 }
-seller_item_info = {
-	"name" = "test_name",
-	"quantity" = "1",
-	"unit_price" = {
-		"currency" = "USD",
-		"value" = "10"
+seller_item_info = [{
+	"name": "test_name",
+	"quantity": "1",
+	"unit_price": {
+		"currency": "USD",
+		"value": "10.00"
 	}
-}
+}]
 seller_note = get_note(0)
-seller_obj = Seller(seller_email, seller_fname, seller_lname, seller_business_name, seller_phone, seller_address, seller_item_info)
+seller_obj = Seller(seller_email, seller_fname, seller_lname, seller_business_name, seller_phone, seller_address, seller_item_info, seller_note)
 
+buyer_billing_info = [{"email": "yabeapplication-buyer-1@gmail.com"}]
+buyer_fname = "Yabe"
+buyer_lname = "Buyer"
+buyer_business_name = ""
+buyer_phone = {
+	"country_code": "001",
+	"national_number": "0987654321"
+}
+buyer_address = {
+	"line1": "345 Chambers Street",
+	"line2": "",
+	"city": "New York",
+	"state": "NY",
+	"postal_code": "10282",
+	"country_code": "US"
+}
+
+buyer_obj = Buyer(buyer_billing_info, buyer_fname, buyer_lname, buyer_business_name, buyer_phone, buyer_address)
+
+create_invoice(seller_obj, buyer_obj, get_token())
