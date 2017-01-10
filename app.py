32f9c409cb33a988f9e8cbee319e60d5c6356c7a
Sebastian Cain <sebastiancain@Sebastians-MacBook-Pro.local>
2017-01-10 14:02:24 -0500

    fixed css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, Jinja2
 import hashlib
 import os
 import utils
