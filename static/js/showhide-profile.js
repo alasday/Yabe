fef9d06108a38cecca339cecd278c2f2857e0101
nfichter <nfichter@stuy.edu>
2017-01-18 10:57:31 -0500

    almost done with showing/hiding form fields, then profile will be finished

---
+++ static/js/showhide-profile.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.showhide-form').hide();
+	
+	$('input[type="checkbox"]').click(function() {
+		if($(this).checked=true) {
+			console.log("#input-{0}".format((this).attr('id')))
+			$("#input-{0}".format((this).attr('id'))).show();
+		}
+		if($(this).checked=false) {
+			$("#input-{0}".format((this).attr('id'))).hide();
+		}
+	});
+});
