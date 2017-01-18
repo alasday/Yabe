9440c477e23c3d8eece5315cff0b7e994399c738
cdelfau <cdelfau@stuy.edu>
2017-01-17 22:23:42 -0500

    looked into some ajaxfeedstuff cou;d be interesting

--- app.py
+++ app.py
@@ -98,6 +98,27 @@ def feed():
         posts = []
         return render_template("feed.html")
 
+#THIS IS A WAY I FOUND (http://flask.pocoo.org/snippets/10/) THAT ALLOWS US TO INCORPORATE AJAX INTO THE FEED
+#from urlparse import urljoin
+#from flask import request
+#from werkzeug.contrib.atom import AtomFeed
+#def make_external(url):
+#    return urljoin(request.url_root, url)
+#@app.route('/recent.atom')
+#def recent_feed():
+#    feed = AtomFeed('Recent Articles',
+#                    feed_url=request.url, url=request.url_root)
+#    articles = Article.query.order_by(Article.pub_date.desc()) \
+#                      .limit(15).all()
+#    for article in articles:
+#        feed.add(article.title, unicode(article.rendered_text),
+#                 content_type='html',
+#                 author=article.author.name,
+#                 url=make_external(article.url),
+#                 updated=article.last_update,
+#                 published=article.published)
+#    return feed.get_response()
+
 #form for item info
 @app.route("/buy")
 def buy():
