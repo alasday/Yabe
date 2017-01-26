d51131d1e9308633d7cf2ae3973bbf307a388844
nfichter <nfichter@stuy.edu>
2017-01-25 21:15:53 -0500

    fixed indentations in app.py

--- app.py
+++ app.py
@@ -17,41 +17,41 @@ f.close()
 #the default page is allows you to choose to login or register
 @app.route("/")
 def loginOrRegister():
-        if "status" in session:
-                redirect("/feed")
-	if 'username' in session:
-	    return render_template("index.html",username = session['username'])
-	else:
-            return render_template("index.html")
+    if "status" in session:
+        redirect("/feed")
+    if 'username' in session:
+        return render_template("index.html",username = session['username'])
+    else:
+        return render_template("index.html")
 
 #register page if you don't already have an account
 @app.route("/register")
 def register():
-        if "status" in session:
-                redirect("/feed")
-	return render_template("register.html")
+    if "status" in session:
+        redirect("/feed")
+    return render_template("register.html")
 
 #login page if you already have an account
 @app.route("/login")
 def login():
-        if "status" in session:
-                redirect("/feed")
-	return render_template("login.html")
+    if "status" in session:
+        redirect("/feed")
+    return render_template("login.html")
 
 #payment page (you have to be logged in to pay for something)
 @app.route("/pay")
 def pay():
-	#item = dbmanager.get_item(item_id)
-	item = {"name":"test", "price":"5.00", "desc":"test_item"}
-	link = paypal.create_payment_for_buyer(0)
-	return render_template("pay.html",link = link, username=session["username"])
+    #item = dbmanager.get_item(item_id)
+    item = {"name":"test", "price":"5.00", "desc":"test_item"}
+    link = paypal.create_payment_for_buyer(0)
+    return render_template("pay.html",link = link, username=session["username"])
 
 #handles input of the login register page
 #authenticates/creates accounts
 @app.route("/authOrCreate", methods=["POST"])
 def authOrCreate():
-        if "status" in session:
-                redirect("/feed")
+    if "status" in session:
+        redirect("/feed")
     formDict = request.form
     if formDict["logOrReg"] == "login":
         username = formDict["username"]
@@ -71,7 +71,7 @@ def authOrCreate():
                 session["status"] = statusNum
                 return redirect("/feed")
 
-		#to be changed
+                #to be changed
         return render_template("loginOrReg.html",status=loginStatus)
 
     elif formDict["logOrReg"] == "register":  #registering
@@ -86,7 +86,7 @@ def authOrCreate():
             registerStatus = "passwords do not match"
         elif statusNum == 2:
             registerStatus = username +" account created"
-	    return redirect("/login") #WE NEED TOPUT AT THE TOP OF THE LOGIN PAGE: 'Username successfully registered, please log in'
+            return redirect("/login") #WE NEED TOPUT AT THE TOP OF THE LOGIN PAGE: 'Username successfully registered, please log in'
         return render_template("loginOrReg.html",status=registerStatus) #status is the login/creation messate 
     else:
         return redirect(url_for("loginOrReg"))
@@ -94,42 +94,42 @@ def authOrCreate():
 #creates a buy request post
 @app.route("/create", methods=["POST"])
 def create():
-        if "status" in session:
-                redirect("/feed")
+    if "status" in session:
+        redirect("/feed")
         #get field stuff
-        pId = dbmanager.new_post(session['username'], request.form["title"], int(request.form["startingPrice"]),int(request.form["period"]))
-        #return redirect('/post/<int: postId>')
-        return redirect(url_for('post',postId = pId))
+    pId = dbmanager.new_post(session['username'], request.form["title"], int(request.form["startingPrice"]),int(request.form["period"]))
+    #return redirect('/post/<int: postId>')
+    return redirect(url_for('post',postId = pId))
 
 #actually posts the buy request post
 @app.route('/post/<int:postId>',methods=["POST","GET"])
 #@app.route("/post/<int: postId>/<int:Period>")
 def post(postId=None):
-        if "status" in session:
-                redirect("/feed")
-	if "Submit" in request.form:
-		dbmanager.new_bid(session['username'],postId,int(request.form.get("amt")))
-	bids = dbmanager.get_bids(postId)
-	startingPrice = dbmanager.get_post(postId)['startingPrice']
-	lowestBid = startingPrice
-	lowestBidId = -1
-	for i in bids:
-		if int(i["price"]) < lowestBid:
-			lowestBid = int(i["price"])
-			lowestBidId = i["id"]
-	return render_template("post.html", username = session['username'], post = dbmanager.get_post(postId), lowestBidId = lowestBidId, lowestBidInfo = dbmanager.get_bid(lowestBidId), allBids = bids)
+    if "status" in session:
+        redirect("/feed")
+    if "Submit" in request.form:
+        dbmanager.new_bid(session['username'],postId,int(request.form.get("amt")))
+    bids = dbmanager.get_bids(postId)
+    startingPrice = dbmanager.get_post(postId)['startingPrice']
+    lowestBid = startingPrice
+    lowestBidId = -1
+    for i in bids:
+        if int(i["price"]) < lowestBid:
+            lowestBid = int(i["price"])
+            lowestBidId = i["id"]
+    return render_template("post.html", username = session['username'], post = dbmanager.get_post(postId), lowestBidId = lowestBidId, lowestBidInfo = dbmanager.get_bid(lowestBidId), allBids = bids)
     
 @app.route("/bid/<int:postId>", methods=["POST","GET"])
 def bid(postId=None):
-	bids = dbmanager.get_bids(postId)
-	startingPrice = dbmanager.get_post(postId)['startingPrice']
-	lowestBid = startingPrice
-	lowestBidId = -1
-	for i in bids:
-		if int(i["price"]) < lowestBid:
-			lowestBid = int(i["price"])
-			lowestBidId = i["id"]
-	return render_template("bid.html", postId = postId, username = session['username'], startingPrice = startingPrice, lowestBidId = lowestBidId, lowestBidInfo = dbmanager.get_bid(lowestBidId), allBids = bids)
+    bids = dbmanager.get_bids(postId)
+    startingPrice = dbmanager.get_post(postId)['startingPrice']
+    lowestBid = startingPrice
+    lowestBidId = -1
+    for i in bids:
+        if int(i["price"]) < lowestBid:
+            lowestBid = int(i["price"])
+            lowestBidId = i["id"]
+    return render_template("bid.html", postId = postId, username = session['username'], startingPrice = startingPrice, lowestBidId = lowestBidId, lowestBidInfo = dbmanager.get_bid(lowestBidId), allBids = bids)
 
 #creates the feed of buy request posts
 @app.route("/feed", methods=["GET", "POST"])
@@ -141,13 +141,13 @@ def feed():
     else:
         # View all posts
         if "username" in session:
-        	username = session["username"]
+            username = session["username"]
         else:
-        	username = ""
+            username = ""
         posts = []
         num = -1
         if "status" in session:
-                num = session["status"]
+            num = session["status"]
                 
     return render_template("feed.html",username=username,posts=dbmanager.get_posts(100),num = num)
 
@@ -156,8 +156,8 @@ def feed():
 #@app.route("/feed/<string: username>/", methods=["GET", "POST"])
 @app.route("/user/<username>/")
 def userposts(username):
-        if "status" in session:
-                redirect("/feed")
+    if "status" in session:
+        redirect("/feed")
     #view all posts by username
     posts = []
     return render_template("user.html",username=username,posts=dbmanager.get_posts_by_username(username))
@@ -247,84 +247,84 @@ def userposts(username):
 #form for item info
 @app.route("/buy")
 def buy():
-        if "status" in session:
-                redirect("/feed")
-	if 'username' in session:
-		return render_template("buy.html", username=session["username"])
-	else:
-		return redirect(url_for('loginOrRegister'))
+    if "status" in session:
+        redirect("/feed")
+    if 'username' in session:
+        return render_template("buy.html", username=session["username"])
+    else:
+        return redirect(url_for('loginOrRegister'))
 
 #form for profile, show specific profile info
 @app.route("/profile", methods=["POST", "GET"])
 def profile():
-        if "status" in session:
-                redirect("/feed")
-	if request.form and not accountManager.full_user_info(session['username']):
-		accountManager.set_user_info(session['username'], request.form.get("email"), request.form.get("addr1"), request.form.get("addr2"), request.form.get("city"), request.form.get("state"), request.form.get("zip"), request.form.get("fname"), request.form.get("lname"), request.form.get("phone"))
-	if 'username' in session:
-		return render_template("profile.html",user_info = accountManager.get_user(session['username']), filled_out = accountManager.full_user_info(session['username']), username=session["username"])
-	else:
-		return redirect(url_for('loginOrRegister'))
+    if "status" in session:
+        redirect("/feed")
+    if request.form and not accountManager.full_user_info(session['username']):
+        accountManager.set_user_info(session['username'], request.form.get("email"), request.form.get("addr1"), request.form.get("addr2"), request.form.get("city"), request.form.get("state"), request.form.get("zip"), request.form.get("fname"), request.form.get("lname"), request.form.get("phone"))
+    if 'username' in session:
+        return render_template("profile.html",user_info = accountManager.get_user(session['username']), filled_out = accountManager.full_user_info(session['username']), username=session["username"])
+    else:
+        return redirect(url_for('loginOrRegister'))
 
 #when updating user info
 @app.route("/profileupdate", methods=["POST"])
 def profileupdate():
-        if "status" in session:
-                redirect("/feed")
-	if 'username' in session and request.form:
-		user_info = accountManager.get_user(session['username'])
-		if request.form.get("inputChangeNameF") == '':
-			nameF = user_info["nameF"]
-		else:
-			nameF = request.form.get("inputChangeNameF")
-			
-		if request.form.get("inputChangeNameL") == '':
-			nameL = user_info["nameL"]
-		else:
-			nameL = request.form.get("inputChangeNameL")
-			
-		if request.form.get("inputChangeEmail") == '':
-			email = user_info["email"]
-		else:
-			email = request.form.get("inputChangeEmail")
-			
-		if request.form.get("inputChangeAddr1") == '':
-			addr1 = user_info["addr1"]
-		else:
-			addr1 = request.form.get("inputChangeAddr1")
-			
-		if request.form.get("inputChangeAddr2") == '':
-			addr2 = user_info["addr2"]
-		else:
-			addr2 = request.form.get("inputChangeAddr2")
-			
-		if request.form.get("inputChangeCity") == '':
-			city = user_info["addrCity"]
-		else:
-			city = request.form.get("inputChangeCity")
-			
-		if request.form.get("inputChangeState") == '':
-			state = user_info["addrState"]
-		else:
-			state = request.form.get("inputChangeState")
-			
-		if request.form.get("inputChangeZip") == '':
-			zip = user_info["addrZip"]
-		else:
-			zip = request.form.get("inputChangeZip")
-			
-		if request.form.get("inputChangePhone") == '':
-			phone = user_info["phone"]
-		else:
-			phone = request.form.get("inputChangePhone")
-		accountManager.set_user_info(session["username"], email, addr1, addr2, city, state, zip, nameF, nameL, phone)
-	return redirect("/profile")
+    if "status" in session:
+        redirect("/feed")
+    if 'username' in session and request.form:
+        user_info = accountManager.get_user(session['username'])
+        if request.form.get("inputChangeNameF") == '':
+            nameF = user_info["nameF"]
+        else:
+            nameF = request.form.get("inputChangeNameF")
+            
+        if request.form.get("inputChangeNameL") == '':
+            nameL = user_info["nameL"]
+        else:
+            nameL = request.form.get("inputChangeNameL")
+            
+        if request.form.get("inputChangeEmail") == '':
+            email = user_info["email"]
+        else:
+            email = request.form.get("inputChangeEmail")
+                
+        if request.form.get("inputChangeAddr1") == '':
+            addr1 = user_info["addr1"]
+        else:
+            addr1 = request.form.get("inputChangeAddr1")
+                        
+        if request.form.get("inputChangeAddr2") == '':
+            addr2 = user_info["addr2"]
+        else:
+            addr2 = request.form.get("inputChangeAddr2")
+                        
+        if request.form.get("inputChangeCity") == '':
+            city = user_info["addrCity"]
+        else:
+            city = request.form.get("inputChangeCity")
+                        
+        if request.form.get("inputChangeState") == '':
+            state = user_info["addrState"]
+        else:
+            state = request.form.get("inputChangeState")
+                        
+        if request.form.get("inputChangeZip") == '':
+            zip = user_info["addrZip"]
+        else:
+            zip = request.form.get("inputChangeZip")
+                
+        if request.form.get("inputChangePhone") == '':
+            phone = user_info["phone"]
+        else:
+            phone = request.form.get("inputChangePhone")
+            accountManager.set_user_info(session["username"], email, addr1, addr2, city, state, zip, nameF, nameL, phone)
+    return redirect("/profile")
 
 #logout of user
 @app.route('/logout', methods=["POST", "GET"])
 def logout():
-        if "status" in session:
-                redirect("/feed")
+    if "status" in session:
+        redirect("/feed")
     if "username" in session:
         session.pop('username')
         render_template("loginOrReg.html",status="logged out")
@@ -335,14 +335,14 @@ def logout():
 #test route for buying (noah)
 @app.route('/buytest', methods=["POST", "GET"])
 def buytest():
-	print paypal.create_payment_for_buyer(0)
-        return "check console"
+    print paypal.create_payment_for_buyer(0)
+    return "check console"
 
 #execute the paypal payment
 @app.route('/buyexec', methods=["POST", "GET"])
 def buyexec():
-	print paypal.execute_payment_for_buyer()
-        return "check paypal"
+    print paypal.execute_payment_for_buyer()
+    return "check paypal"
 
 #run the app
 if __name__ == "__main__":
